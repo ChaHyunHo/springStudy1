3f752132f55ea1768698ad02b958ea85a514aa48
CHAHYUNHO <chamym@naver.com>
2020-05-27 10:32:44 +0900

    생명주기(Life Cycle)

--- src/main/java/com/brms/book/service/BookRegisterService.java
+++ src/main/java/com/brms/book/service/BookRegisterService.java
@@ -1,12 +1,15 @@
 package com.brms.book.service;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.brms.book.Book;
 import com.brms.book.dao.BookDao;
 
-public class BookRegisterService {
-
+public class BookRegisterService implements InitializingBean, DisposableBean {
+	// InitializingBean, DisposableBean 생성 및 소멸 시점에서의 특정작업을 할 수 있음
+	
 	@Autowired
 	private BookDao bookDao;
 	
@@ -22,5 +25,17 @@ public class BookRegisterService {
 	
 	public void destroyMethod() {
 		System.out.println("BookRegisterService 빈(Bean)객체 소멸 단계");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Bean 객체 생성");
+
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Bean 객체 소멸");
+		
 	}
 }
--- src/main/java/com/brms/book/service/BookSearchService.java
+++ src/main/java/com/brms/book/service/BookSearchService.java
@@ -1,11 +1,13 @@
 package com.brms.book.service;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.brms.book.Book;
 import com.brms.book.dao.BookDao;
 
-public class BookSearchService {
+public class BookSearchService implements InitializingBean, DisposableBean {
 
 	@Autowired
 	private BookDao bookDao;
@@ -16,5 +18,18 @@ public class BookSearchService {
 		Book book = bookDao.select(bNum);
 		return book;
 	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Bean 객체 생성");
+		
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Bean 객체 소멸");
+		
+	}
+
 	
 }
